950d19d05cf73b97316b88b46b44179786db2f33
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-13 18:00:39 +0200

    fix: rebase
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ storybook/src/stories/Tooltip.stories.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+import { Tooltip } from '@podman-desktop/ui-svelte';
+import { Story, Template } from '@storybook/addon-svelte-csf';
+
+export const meta = {
+  title: 'Example/Tooltip',
+  component: Tooltip,
+  tags: ['autodocs'],
+};
+</script>
+
+<Template let:args>
+  <Tooltip {...args} />
+</Template>
+
+<Story name="Default"></Story>
