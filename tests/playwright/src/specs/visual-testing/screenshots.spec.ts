a8eaf7ebfc0db3c09006e7f271784f910f9c26b7
axel7083 <42176370+axel7083@users.noreply.github.com>
2026-06-11 15:14:53 +0200

    feat: screenshots
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- tests/playwright/src/specs/visual-testing/screenshots.spec.ts
+++ tests/playwright/src/specs/visual-testing/screenshots.spec.ts
@@ -16,12 +16,90 @@ test.describe
       'Skipping screenshots if PLAYWRIGHT_SCREENSHOTS_PATH is not set.',
     );
 
-    test('simple screenshot', async ({ navigationBar }) => {
-      const dashboard = await navigationBar.openDashboard();
-      await playExpect(dashboard.heading).toBeVisible();
+    test('dashboard screenshot', async ({ navigationBar }) => {
+      const dashboardPage = await navigationBar.openDashboard();
+      await playExpect(dashboardPage.heading).toBeVisible();
 
-      await dashboard.screenshot({
+      // focus on the content
+      await dashboardPage.content.focus();
+
+      await dashboardPage.screenshot({
         name: 'dashboard',
+      });
+    });
+
+    /**
+     * Containers
+     */
+    test('containers screenshot', async ({ navigationBar }) => {
+      const containersPage = await navigationBar.openContainers();
+      await playExpect(containersPage.heading).toBeVisible();
+
+      // focus on the content
+      await containersPage.content.focus();
+
+      await containersPage.screenshot({
+        name: 'containers-empty',
+      });
+    });
+
+    /**
+     * Pods
+     */
+    test('pods screenshot', async ({ navigationBar }) => {
+      const podsPage = await navigationBar.openPods();
+      await playExpect(podsPage.heading).toBeVisible();
+
+      // focus on the content
+      await podsPage.content.focus();
+
+      await podsPage.screenshot({
+        name: 'pods-empty',
+      });
+    });
+
+    /**
+     * Images
+     */
+    test('images screenshot', async ({ navigationBar }) => {
+      const imagesPage = await navigationBar.openImages();
+      await playExpect(imagesPage.heading).toBeVisible();
+
+      // focus on the content
+      await imagesPage.content.focus();
+
+      await imagesPage.screenshot({
+        name: 'images-empty',
+      });
+    });
+
+    /**
+     * Volumes
+     */
+    test('images screenshot', async ({ navigationBar }) => {
+      const volumesPage = await navigationBar.openVolumes();
+      await playExpect(volumesPage.heading).toBeVisible();
+
+      // focus on the content
+      await volumesPage.content.focus();
+
+      await volumesPage.screenshot({
+        name: 'volumes-empty',
+      });
+    });
+
+    /**
+     * Networks
+     */
+    test('images screenshot', async ({ navigationBar }) => {
+      const networksPage = await navigationBar.openNetworks();
+      await playExpect(networksPage.heading).toBeVisible();
+
+      // focus on the content
+      await networksPage.content.focus();
+
+      await networksPage.screenshot({
+        name: 'networks-empty',
       });
     });
   });
